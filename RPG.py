c9a82bffeb329c61d5a7f67535f07202c9ac1668
hmacadura <henriquemacadura@gmail.com>
2018-03-16 01:52:13 -0300

    Classes atualizadas

--- RPG.py
+++ RPG.py
@@ -17,7 +17,7 @@ nome= str(input("Digite seu nome: "))
 sexo= int(input("Digite '1' para sexo masculino ou '2' para feminino "))
           
 classe = int(input("\nBem vindo ao menu de seleção de classes."
-                   "\nPara selecionar apenas digite o número referente à classe:\n \nMago 1 \nGuerreiro 2 \nArqueiro 3 "))
+                   "\nPara selecionar apenas digite o número referente à classe:\n \nMago 1 \nGuerreiro 2 \nArqueiro 3 \nGatuno 4 \nBardo 5 \n"))
                   
 
 ##########################################
@@ -29,7 +29,7 @@ if sexo == 1 and classe == 1:
     vitalidade = 3
     inteligencia = 8
     agilidade = 3
-    carisma = 4
+    carisma = 7
     print("\nStatus: ",
           "\nForça: ",forca,
           "\nVitalidade: ",vitalidade,
@@ -44,7 +44,7 @@ elif sexo == 2 and classe == 1:
     vitalidade = 3
     inteligencia = 8
     agilidade = 3
-    carisma = 4
+    carisma = 7
     print("\nStatus: ",
           "\nForça: ",forca,
           "\nVitalidade: ",vitalidade,
@@ -121,3 +121,80 @@ elif sexo == 2 and classe == 3:
 #ARQUEIRO
 
 ##########################################
+    
+#GATUNO
+elif sexo == 1 and classe == 4:
+    print("\n",nome, ", o Gatuno")
+    forca = 3
+    vitalidade = 5
+    inteligencia = 5
+    agilidade = 9
+    carisma = 4
+    print("\nStatus: ",
+          "\nForça: ",forca,
+          "\nVitalidade: ",vitalidade,
+          "\nInteligência: ",inteligencia,
+          "\nAgilidade: ",agilidade,
+          "\nCarisma: ",carisma)
+
+
+
+elif sexo == 2 and classe == 4:
+    print("\n",nome ,", a Gatuna")
+    forca = 3
+    vitalidade = 5
+    inteligencia = 5
+    agilidade = 9
+    carisma = 4
+    print("\nStatus: ",
+          "\nForça: ",forca,
+          "\nVitalidade: ",vitalidade,
+          "\nInteligência: ",inteligencia,
+          "\nAgilidade: ",agilidade,
+          "\nCarisma: ",carisma)
+#GATUNO
+
+##########################################
+    
+#BARDO
+if sexo == 1 and classe == 5:
+    print("\n",nome, ", o Bardo")
+    forca = 2
+    vitalidade = 3
+    inteligencia = 5
+    agilidade = 3
+    carisma = 9
+    print("\nStatus: ",
+          "\nForça: ",forca,
+          "\nVitalidade: ",vitalidade,
+          "\nInteligência: ",inteligencia,
+          "\nAgilidade: ",agilidade,
+          "\nCarisma: ",carisma)
+
+    
+elif sexo == 2 and classe == 5:
+    print("\n",nome ,", a Barda")
+    forca = 2
+    vitalidade = 3
+    inteligencia = 5
+    agilidade = 3
+    carisma = 9
+    print("\nStatus: ",
+          "\nForça: ",forca,
+          "\nVitalidade: ",vitalidade,
+          "\nInteligência: ",inteligencia,
+          "\nAgilidade: ",agilidade,
+          "\nCarisma: ",carisma)
+          
+#BARDO
+
+##########################################
+
+print("Seu personagem está pronto!\n")
+
+print("Bem-vindo ao mundo medieval de (Nome a decidir),", nome)
+
+
+
+
+
