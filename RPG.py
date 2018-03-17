32ba3d675153ad7cbbc142926b803ed7dafdac31
hmacadura <henriquemacadura@gmail.com>
2018-03-17 15:27:54 -0300

    Início atualizado

--- RPG.py
+++ RPG.py
@@ -11,7 +11,7 @@ comecar = str(input('Aperte "ENTER" para começar o jogo '))
 
 if comecar == "":
     print("\n\nVocê está prestes a vivenciar uma experiência única!"
-          "\n\nMas antes, um pequeno tutorial"
+          "\n\nMas antes, um pequeno tutorial:"
           "\nO <jogo> é narrado, e opções de diálogo são dadas ao protagonista."
           " Para escolhe-las, digite apenas o número que"
           " corresponda a opção desejada."
@@ -29,7 +29,7 @@ sexo= int(input("\nDigite (1) para sexo masculino ou (2) para feminino: "))
           
 classe = int(input("\nBem vindo ao menu de seleção de classes."
                    "\nPara selecionar apenas digite o número referente à classe:"
-                   "\n(1) Mago: Os oráculos de Macadura  "
+                   "\n(1) Mago: Os Oráculos de Hmacadura  "
                    "\n(2) Guerreiro: Os Guerreiros de Lepercia"
                    "\n(3) Arqueiro: Os Discipulos de Vicasa  \n"))
 
@@ -38,13 +38,13 @@ classe = int(input("\nBem vindo ao menu de seleção de classes."
 
 #MAGO
 if sexo == 1 and classe == 1:
-    print("\n",nome, ",o Oraculo de Macadura")
+    print("\n",nome, ",o Oráculo de Hmacadura")
     mentor = " Hmacadema"
 
 
     
 elif sexo == 2 and classe == 1:
-    print("\n",nome ,",o Oraculo de Macadura")
+    print("\n",nome ,",a Oráculo de Hmacadura")
     mentor = " Hmacadema"
 
           
@@ -90,7 +90,6 @@ print("\nSeu personagem está pronto!\n")
 
 if sexo == 1 and classe == 1:
     classe1= ",o Mago"
-    print(nome,classe1)
     print("\nBem-vindo ao mundo medieval de (Nome a decidir)", nome, classe1,
           ".\nEsteja ciente que suas escolhas irão formar seu futuro, então escolha com sabedoria.")
 
@@ -136,15 +135,36 @@ elif sexo == 2 and classe == 3:
 print("\n\n\nHá muito tempo atrás, uma maldição fora lançada para o Reino(nome)"
       "por uma Bruxa muito poderosa."
       "\nSabendo da maldição, o rei e a rainha chamaram as três lendas mais"
-      " poderoas de Reino: \n\nHmacadura, o sábio Mago"
-      "\nVicasa, o ágil Arqueiro"
-      "\ne Lepercia, o grande Guerreiro."
-      "\n\nDeram aos três então, a tarefa de levar seu recém-nascido filho"
+      " poderoas do Reino: \n\n-Hmacadura, o sábio Mago"
+      "\n-Vicasa, o ágil Arqueiro"
+      "\n-Lepercia, o grande Guerreiro."
+      "\n\nDeram aos três então, a tarefa de levar seu bebê"
       " para um lugar fora do alcance da maldição"
-      ",ate que um dia seja seguro retornar."
-      "\n\n\n\n\nEntão...\n\n")
-#########INÍCIO
+      ",até que um dia seja seguro retornar.")
+      
+if sexo == 1 and classe == 1:
+    print("As três lendas cumpriram a ordem do rei.\nMas o herdeiro criou um vínculo com o Mago, e já sendo propício às artes místicas, resolveu treina-lo e molda-lo a sua imagem."
+          "\n\n\n\n\n25 anos depois...\n\n")
 
+elif sexo == 2 and classe == 1:
+    print("As três lendas cumpriram a ordem do rei.\nMas a herdeira criou um vínculo com o Mago, e já sendo propícia às artes místicas, resolveu treina-la e molda-la a sua imagem."
+          "\n\n\n\n\n25 anos depois...\n\n")
+
+elif sexo == 1 and classe == 2:
+    print("As três lendas cumpriram a ordem do rei.\nMas o herdeiro criou um vínculo com o Guerreiro, e já que era forte fisicamente e determinada, resolveu treina-lo e molda-lo a sua imagem. "
+          "\n\n\n\n\n25 anos depois...\n\n")
+    
+elif sexo == 2 and classe == 2:
+    print("As três lendas cumpriram a ordem do rei.\nMas a herdeira criou um vínculo com o Guerreiro, e já que era forte fisicamente e determinada, resolveu treina-la e molda-la a sua imagem. "
+          "\n\n\n\n\n25 anos depois...\n\n")
+    
+elif sexo == 1 and classe == 3:
+    print("As três lendas cumpriram a ordem do rei.\nMas o herdeiro criou um vínculo com o Arqueiro, e já que possuía certa agilidade e uma boa mira, resolveu treina-lo e molda-lo a sua imagem. "
+          "\n\n\n\n\n25 anos depois...\n\n")
+
+elif sexo == 2 and classe == 3:
+    print("As três lendas cumpriram a ordem do rei.\nMas a herdeira criou um vínculo com o Arqueiro, e já que possuía certa agilidade e uma boa mira, resolveu treina-la e molda-la a sua imagem.  "
+          "\n\n\n\n\n25 anos depois...\n\n")
 #########MAGO
 if classe == 1:
     print(nome,"estava lendo na antiga biblioteca, quando então um servo veio trazer uma mensagem:",
@@ -173,32 +193,32 @@ else:
           "\n\n-Olá",mentor,"! Queria me ver?"
           "\n\n-Entre",nome,",há algo que preciso contar.")
 
-dialogo = int(input("\n-Ora, o que é dessa vez? Lembre-se que não tenho tempo para rumores bobos!(1)"
-                    "\n\n-O que me conta? Noticias do Reino?(2)"))
+dialogo = int(input("\n-Ora, o que é dessa vez? Lembre-se que não tenho tempo para rumores bobos!(1)\n"
+                    "\n\n-O que me conta? Noticias do Reino?(2)\n"))
 ########MAGO
 if dialogo == 1 and classe == 1:
     print("\n-Não seja arrogante",nome,". Talvez passar muito tempo com os livros tenha"
-          " te feito esquecer como ter modos.")
+          " te feito esquecer como ter modos.\n")
 elif dialogo == 2 and classe ==1:
-    print("\n-Pergaminhos voaram do Reino, está na hora de retomar o que é seu!")
+    print("\n-Pergaminhos voaram do Reino, está na hora de retomar o que é seu!\n")
 ########MAGO
 
-########MAGO
+########GUERREIRO
 if dialogo == 1 and classe == 2:
-    print("\n-Um guerreiro nao tem apenas força bruta, mas sabe o que é ter respeito, e isto lhe falta.")
+    print("\n-Um guerreiro nao tem apenas força bruta, mas sabe o que é ter respeito, e isto lhe falta.\n")
 elif dialogo == 2 and classe ==2:
-    print("\n-Noticias do Reino. A hora de combater as forças malignas chegou.")
-########MAGO
+    print("\n-Noticias do Reino. A hora de combater as forças malignas chegou.\n")
+########GUERREIRO
 
 
 #########ARQUEIRO
 elif dialogo == 1 and classe == 3:
-              print("\n\n-Está esquecendo uma das maiores virtudes",nome,"? A pressa é a maior inimiga da precisão!")
+              print("\n\n-Está esquecendo uma das maiores virtudes",nome,"? A pressa é a maior inimiga da precisão!\n")
 elif dialogo == 2 and classe == 3:
-              print("\n\n-Você já é grande,",nome,"! Está na hora de retomar o que é seu!")
+              print("\n\n-Você já é grande,",nome,"! Está na hora de retomar o que é seu!\n")
 #########ARQUEIRO  
 
-
+#########INÍCIO
 
 
 
