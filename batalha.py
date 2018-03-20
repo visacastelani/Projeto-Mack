86635bb9502ae7449df8297169fe1309b40273ce
visacastelani@gmail.com <visacastelani@gmail.com>
2018-03-20 16:50:48 -0300

    Aprimorado o sistema de batalha - batalha por rodadas

--- batalha.py
+++ batalha.py
@@ -6,33 +6,45 @@ contador1= 0
 hp = 10
 enemyHP = 10
 
+lutar = str(input("\n\nDigite 'LUTAR' para comaçar o duelo!"))
+lutar = lutar.upper()
 
-while hp >0 or enemyHP >0:
-    
-    if hp >0 or enemyHP >0:
+while lutar != "LUTAR":
+        lutar = str(input("\n\nDigite 'LUTAR' para comaçar o duelo!"))
+        lutar = lutar.upper()
 
-        #DANO RECEBIDO
-        dmg = randint(0,2)
-        hp = hp - dmg
-        
-        #DANO CAUSADO
-        dmg1 = randint(0,3)
-        enemyHP = enemyHP - dmg1
+if lutar == "LUTAR":
 
-        print("\nO inimigo irá atacar, defenda-se!")
-        print("\nDMG: ",dmg,
-            "\nHP: " ,hp)
-        
-        print("\nSua vez de atacar!")
-        print("\nDMG no Monstro: ", dmg1,
-              "\nHP do Monstro:", enemyHP,)
+        while hp > 0 or enemyHP > 0:
+                
+                print("\nO inimigo irá atacar, defenda-se!")
+                #DANO RECEBIDO
+                dmg = randint(0,2)
+                hp = hp - dmg
+                print("\n####   DANO RECEBIDO: ",dmg,
+                "\n####   HP ATUAL:      " ,hp)
+                if  hp == 0 or hp < 0:
+                        print("\n\nVocê esta morto!")
+                        break
+                
+                           
+                                            
+                print("\nSua vez de atacar!")
+                #DANO CAUSADO
+                dmg1 = randint(0,3)
+                enemyHP = enemyHP - dmg1
+                print("\n####   DANO CAUSADO: ", dmg1,
+                "\n####   HP DO MONSTRO:", enemyHP)
+
+                input("\n\nAperte 'ENTER' para ir para a"
+                                   " próxima rodada!")
+
+                
+
+                if enemyHP == 0 or enemyHP < 0:
+                        print("\n\nVocê derrotou o inimigo!")
+                        break
+                
 
 
-    if  hp == 0 or hp < 0:
-        print("Você esta morto!")
-        break
-
-    elif enemyHP == 0 or enemyHP < 0:
-        print("Você derrotou o inimigo!")
-        break
-    input()
+input()
