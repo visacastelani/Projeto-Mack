ea2b7726fdeb9971ad4052077139d4ce837d4825
visacastelani@gmail.com <visacastelani@gmail.com>
2018-03-20 16:53:37 -0300

    Algoritmo de Opção - STEALTH

--- stealth.py
+++ stealth.py
@@ -51,31 +51,35 @@ if opcao == "STEALTH":
 
 elif opcao =="LUTAR":
     print("\n\nVocê decidiu Lutar!")
-    while hp >0 or enemyHP >0:
-    
-        if hp >0 or enemyHP >0:
+    while hp > 0 or enemyHP > 0:
+                
+                print("\nO inimigo irá atacar, defenda-se!")
+                #DANO RECEBIDO
+                dmg = randint(0,2)
+                hp = hp - dmg
+                print("\n####   DANO RECEBIDO: ",dmg,
+                "\n####   HP ATUAL:      " ,hp)
+                if  hp == 0 or hp < 0:
+                        print("\n\nVocê esta morto!")
+                        break
+                
+                           
+                                            
+                print("\nSua vez de atacar!")
+                #DANO CAUSADO
+                dmg1 = randint(0,3)
+                enemyHP = enemyHP - dmg1
+                print("\n####   DANO CAUSADO: ", dmg1,
+                "\n####   HP DO MONSTRO:", enemyHP)
 
-            #DANO RECEBIDO
-            dmg = randint(0,2)
-            hp = hp - dmg
-            #DANO CAUSADO
-            dmg1 = randint(0,3)
-            enemyHP = enemyHP - dmg1
-            print("\nO inimigo irá atacar, defenda-se!")
-            print("\nDMG: ",dmg,
-                  "\nHP: " ,hp)
+                input("\n\nAperte 'ENTER' para ir para a"
+                                   " próxima rodada!")
 
-            print("\nSua vez de atacar!")
-            print("\nDMG no Monstro: ", dmg1,
-                  "\nHP do Monstro:", enemyHP,)
+                
 
-        if  hp == 0 or hp < 0:
-            print("\n\n\n\nVocê esta morto!")
-            break
-
-        elif enemyHP == 0 or enemyHP < 0:
-            print("\n\n\n\nVocê derrotou o inimigo!")
-            break
+                if enemyHP == 0 or enemyHP < 0:
+                        print("\n\nVocê derrotou o inimigo!")
+                        break
     input()
 
                 
