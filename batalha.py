7060faa09f7098e03addea291dfa5de3e6182f3b
visacastelani@gmail.com <visacastelani@gmail.com>
2018-03-24 12:37:11 -0300

    ALGORITMO APRIMORADO

--- batalha.py
+++ batalha.py
@@ -10,21 +10,16 @@ while lutar != "LUTAR":
         lutar = lutar.upper()
 
 if lutar == "LUTAR":
-
         while hp > 0 or enemyHP > 0:
                         
-                        print("\nO inimigo irá atacar, defenda-se!")
+                print("\nO inimigo irá atacar, defenda-se!")
                         #DANO RECEBIDO
                         dmg = randint(0,2)
                         hp = hp - dmg
                         print("\n####   DANO RECEBIDO: ",dmg,
                         "\n####   HP ATUAL:      " ,hp)
-                        if  hp == 0 or hp < 0:
-                                print("\n\nA batalha foi árdua, você lutou"
-                                      " bravamente, mas infelizmente"
-                                      " as forças das trevas venceram."
-                                      "\n\nVocê esta morto!")
-                                break
+                        
+                                
                         
                                    
                                                     
@@ -38,8 +33,14 @@ if lutar == "LUTAR":
                         input("\n\nAperte 'ENTER' para ir para a"
                                            " próxima rodada!")
 
+                        if  hp == 0 or hp < 0:
+                                print("\n\nA batalha foi árdua, você lutou"
+                                      " bravamente, mas infelizmente"
+                                      " as forças das trevas venceram."
+                                      "\n\nVocê esta morto!")
+
                         
 
-                        if enemyHP == 0 or enemyHP < 0:
+                        elif enemyHP == 0 or enemyHP < 0:
                                 print("\n\nVocê derrotou o inimigo!")
                                 break
